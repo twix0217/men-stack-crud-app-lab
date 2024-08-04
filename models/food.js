3df51ec7d75b9bf815c02b83f5d3cfd811976d45
twix217 <hussainbarakat02@gmail.com>
2024-08-04 14:37:32 +0300

    push

--- models/food.js
+++ models/food.js
@@ -1,12 +1,10 @@
 //Mongoose connect
 const mongoose = require("mongoose");
 
-
 const foodSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true},
-  
-})
+  name: String,
+  isReadyToEat: Boolean,
+});
 
 const food = mongoose.model('food', foodSchema)
 module.exports = food
